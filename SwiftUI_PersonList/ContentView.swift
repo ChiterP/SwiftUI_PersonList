281bd1ca682bac21d7b4974c5266904e629fe9ce
ChiterP <63542970+ChiterP@users.noreply.github.com>
2021-06-15 11:04:42 +0300

    Initial Commit

---
+++ SwiftUI_PersonList/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI_PersonList
+//
+//  Created by 18316409 on 15.06.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUI_PersonList/SwiftUI_PersonListApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_PersonListApp.swift
+//  SwiftUI_PersonList
+//
+//  Created by 18316409 on 15.06.2021.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_PersonListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
